680910b2e14a6ca8d13b661dc0a75ecaafeaa42f
Kambou Sheila <shei.kambou16@gmail.com>
2022-12-05 23:54:53 -0800

    Exercice XP

---
+++ week4/day1/exercice/exerciceXP.py
@@ -0,0 +1,49 @@
+#Exercice1#
+print("Hello world\n"*4)
+
+#Exercice2
+x=(99^3) * 8
+print(x)
+
+#Exercice3#
+#1)  False#
+#2)  True#
+#3)  False#
+#4)  Il y'aura une erreur car on ne peut comparer une chaine de caractère et un chiffre#
+#5)  False#
+
+#Exercice4#
+computer_brand="ASUS"
+print("I have a "+computer_brand+" computer")
+
+#Exercice5#
+name="Kambou"
+age="20ans"
+shoe_size="41"
+info="Je m'appelle "+name+" ,j'ai "+age+" et je chausse du "+shoe_size
+print(info)
+
+#Exercice6#
+a=12
+b=11
+if a>b:
+    print("Hello world")
+
+#Exercice7#
+m=input("Entrez un nombre")
+
+
+
+#Exercice8#
+n="Harry"
+m=input("quel est votre nom?")
+if n==m:
+    print("Wouah on a le même nom!!!")
+
+
+#Exercice9"
+x=input("Quelle est votre taille?")
+if x>145:
+    print("Vous avez atteint la taille pour pouvoir rouler")
+else
+    print("Vous devez attendre encore un peu")
