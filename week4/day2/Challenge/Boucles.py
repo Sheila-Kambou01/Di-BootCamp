27c2d45d98ca2206aacb509034e7baef38d896d4
Kambou Sheila <shei.kambou16@gmail.com>
2022-12-30 10:09:57 -0800

    restauration challenge

---
+++ week4/day2/Challenge/Boucles.py
@@ -0,0 +1,10 @@
+#Exercice1
+f=int(input("Vous voulez les multiples de quels nombres? "))
+t=int(input("Combien de fois? "))
+l=[]
+for i in range(0,t):
+    f=f*2
+    l.append(f)
+print(l)
+
+#Exercice2
